d11715dd5aaf2e102274c9bf8dcc4054b5ef1025
beevekmgr <beevekmgr@gmail.com>
2021-08-31 18:57:06 +0545

    feat: List view for trsnaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,47 +8,63 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  width: 2,
-                  color: Colors.purple,
-                )),
-                child: Text(
-                  '\$${tx.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
+    return Container(
+        height: 300,
+        child: transactions.isEmpty
+            ? Column(
+                children: <Widget>[
+                  Text("No Transactions added yet!"),
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-              ),
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tx.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 18,
-                      ),
+                  Container(
+                    height: 200,
+                    child: Image.asset(
+                      "assets/images/waiting.png",
+                      fit: BoxFit.cover,
                     ),
-                    Text(
-                      DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(color: Colors.red),
-                    )
-                  ])
-            ],
-          ),
-        );
-      }).toList(),
-    );
+                  ),
+                ],
+              )
+            : ListView(
+                children: transactions.map((tx) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 50),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            width: 2,
+                            color: Theme.of(context).primaryColor,
+                          )),
+                          child: Text(
+                            '\$${tx.amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                tx.title,
+                                style: Theme.of(context).textTheme.title,
+                              ),
+                              Text(
+                                DateFormat.yMMMd().format(tx.date),
+                                style: TextStyle(color: Colors.red),
+                              )
+                            ])
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ));
   }
 }
